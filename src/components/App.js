e9d86fc8b74e25e0d0e49afa62f167e8211026c5
Gri25 <grigorijabrosimov@gmail.com>
2021-12-24 03:35:53 +0500

    feat: work 12.3

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Profile from "./profile/Profile";
 import Register from "./register/Register ";
 import Login from "./login/Login ";
 //import PageNotFound from "./PageNotFound";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 //import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import ProtectedRoute from "./ProtectedRoute";
 import * as auth from "./auth.js";
@@ -16,66 +16,74 @@ const App = () => {
     password: "",
   });
 
-  /*
-  componentDidMount() {
-    this.tokenCheck();
-    // проверить токен пользователя
-  }
-  */
+  const history = useHistory()
+
+  useEffect(() => {
+    if (loggedIn === true) {
+      history.push('/profile')
+    }
+  }, [loggedIn])
+
+  useEffect(() => {
+    tokenCheck()
+  }, [])
 
   const tokenCheck = () => {
-    /*
+    
     // если у пользователя есть токен в localStorage,
     // эта функция проверит валидность токена
-    const jwt = localStorage.getItem("jwt");
-    if (jwt) {
+    const token = localStorage.getItem("token");
+    if (token) {
       // проверим токен
-      auth.getContent(jwt).then((res) => {
-        if (res) {
+      auth.getContent(token).then((data) => {
+        if (data) {
           // здесь можем получить данные пользователя!
           const userData = {
-            password: res.password,
-            email: res.email,
+            email: data.email,
+            password: data.password,
           };
-          // авторизуем пользователя
-          this.setState(
-            {
-              loggedIn: true,
-              userData, // поместим их в стейт внутри App.js
-            },
-            () => {
-              // обернём App.js в withRouter
-              // так, что теперь есть доступ к этому методу
-              this.props.history.push("/profile");
-            }
-          );
+          localStorage.setItem("token", token)
+          setUserData(userData)
+          setLoggedIn(true)
         }
       });
-    } */
+    } 
   };
 
   const handleLogin = ({ email, password }) => {
     console.log(email, password);
     // e.preventDefault(); если будешь писать то дбавь (e) как аргумент
+    auth.authorize({
+      email,
+      password
+    })
+    .then(data => console.log(data))
+  //  .then(token => {
+    //  if (token) {
+     //  setLoggedIn(true);не понятно поему не меняется
+   //   }
+   // })
+    .catch(error => console.error(error))
   };
 
   const handleRegister = ({ email, password }) => {
     console.log(email, password);
     auth
       .register({ email, password })
-      .then(res => console.log(res))
-      /*
+      .then((res) => console.log(res))
+      
       .then((data) => {
-        if (data.jwt) {
-          const { jwt, email, password } = data;
-          localStorage.setItem("jwt", jwt);
+        if (data) {
+          const { email, password } = data;
+        //  localStorage.setItem("token", token);
           setUserData({
             email,
             password,
           });
-          setLoggedIn(true);
+         // setLoggedIn(true);тут вроде бы как это не нужно писать
         }
-      }) */
+      }) 
+      
       .catch((error) => console.error(error));
   };
 
